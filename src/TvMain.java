f8521fdf4e6c6019fb4c74fac179119a8964d482
Tea Andersson <tea.andersson@elev.ga.ntig.se>
2024-10-23 11:20:51 +0200

    försökt att fixa readFromFile men lyckas inte riktigt...

--- src/TvMain.java
+++ src/TvMain.java
@@ -1,28 +1,52 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TvMain{
     private ArrayList<TvSeries> shows = new ArrayList<>();
+    
+    public TvMain(){
+    Scanner sc = new Scanner(System.in);
+
+    //File myObj = new File("myTvSeries.ser");
+
+    readFromFile();
+
+    }
    
-    public void readFromFile(){
+    private void readFromFile(){
         TvSeries tvSeries1 = null;
         TvSeries tvSeries2 = null;
-        
         
         try {
             FileInputStream fileIn = new FileInputStream("myTvSeries.ser"); //resposible for reading from the file
             ObjectInputStream objectIn = new ObjectInputStream(fileIn); //Responsible for building the objects
 
             //read from file
-            tvSeries1 = (TvSeries) objectIn.readObject();
-            tvSeries2 = (TvSeries) objectIn.readObject();
+            //shows = (ArrayList<TvSeries>) objectIn.readObject();
+            tvSeries1 = (TvSeries)objectIn.readObject();
+            tvSeries2 = (TvSeries)objectIn.readObject();
+            
 
             //close the streams
             fileIn.close();
             objectIn.close();
-        } catch (Exception error){
+
+        } catch (IOException error){
             System.out.println("Could not read that...");
+        } catch (ClassNotFoundException e){
+            System.out.println("That class does not exist");
+        }
+
+        //shows.printInfo();
+        try {
+            shows.get(0).printInfo();
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+            System.out.println("There are no shows");
         }
 
         tvSeries1.printInfo();
@@ -31,7 +55,22 @@ public class TvMain{
 
 
     public void write2File(){
+        TvSeries tvSeries1 = new TvSeries("TV1");
+        TvSeries tvSeries2 = new TvSeries("TV2");
 
+        try {
+            FileOutputStream fileOut = new FileOutputStream("myTvSeries.ser");
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+
+            objectOut.writeObject(shows);
+
+            fileOut.close();
+            objectOut.close();
+
+            System.out.println("I have written");
+        } catch (IOException error) {
+            System.out.println("Could not write to file");
+        }
     }
 }
 
--- src/TvSeries.java
+++ src/TvSeries.java
@@ -1,29 +1,38 @@
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class TvSeries {
+public class TvSeries implements Serializable{
     private String name;
     private ArrayList<Integer> episodes = new ArrayList<>();
     private int rating = 0;
+    Scanner sc = new Scanner(System.in);
 
 
-    TvSeries(String name, int rating){
+   /*  TvSeries(String name, int rating){
         this.name = name;
         this.rating = rating;
         episodes.add(1);
+    }*/
+
+    public TvSeries() {
+        System.out.println("What is the name of the TV series? ");
+        String inName = sc.nextLine();
     }
 
-    TvSeries(String Inname) {
-        
-
+    public TvSeries(String inName) {
+        name = inName;
     }
 
     public void addEpisodes(int numOfEpisodes, int season){
-
+        episodes.add(1,1);
     }
 
-    public void changeRating(){
-
+    public void changeRating(int newRating){
+        System.out.println("What would you like to rate the series? ");
+        newRating = sc.nextInt();
+        rating = newRating;
     }
 
     public void printInfo(){
